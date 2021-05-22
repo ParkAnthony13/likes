3c849ae65ccf9436efa1e83ae5e53110225c106d
Anthony Park <anth0nypark13@gmail.com>
2021-05-21 19:11:00 -0700

    First Upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var count = 0;
+var countElement = document.querySelector(".counter");
+
+function countLike() {
+    count++;
+    countElement.innerText = count + " likes";
+    console.log(count);
+}
+
+var count2 = 0;
+var countElement2 = document.querySelector(".counter2");
+
+function countLike2() {
+    count2++;
+    countElement2.innerText = count2 + " likes";
+    console.log(count2);
+}
+
+var count3 = 0;
+var countElement3 = document.querySelector(".counter3");
+
+function countLike3() {
+    count3++;
+    countElement3.innerText = count3 + " likes";
+    console.log(count3);
+}
